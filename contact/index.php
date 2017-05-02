3803e92b49632add9024cec953814b727d139b49
Sean Pianka <pianka@eml.cc>
2017-05-01 21:42:01 -0400

    removed unnecessary instantiation of twig renderer

--- contact/index.php
+++ contact/index.php
@@ -4,9 +4,6 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/includes/auth.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/includes/products.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/includes/twig.php');
 
-$loader = new Twig_Loader_Filesystem('../templates/');
-$twig = new Twig_Environment($loader);
-
 $template = $twig->render(
     "frontpage/contact.html.twig", array(
         "product_categories" => $product_categories,
